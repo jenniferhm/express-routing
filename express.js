e588021973b909d3087b5ca9273137cde0c3f4c3
Jennifer Ma <jenniferma93@gmail.com>
2019-09-04 12:13:27 -0700

    added mean route

--- express.js
+++ express.js
@@ -8,9 +8,57 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true}));
 
+app.get("/mean", function(req, res, next) {
+  let numsArr = (req.query.numbers).split(",");
+  let convertedToNums = numsArr.map(strNum => {
+    if (isNaN(+strNum)) return console.log("Please only enter numbers!");
+    return +strNum;
+  });
+  console.log(_calculateMean(convertedToNums));
+})
 
+function _calculateMean(arr) {
+  let sum = arr.reduce((total, num) => total + num);
+  let mean = sum/arr.length;
+  return +mean.toFixed(2);
+}
 
+function _calculateMedian(arr) {
+  let sortedArr = arr.sort((a, b) => a-b);
 
+  if (sortedArr.length % 2 !== 0) {
+    let i = Math.floor(sortedArr.length / 2);
+    return sortedArr[i];
+  } else {
+    let left = Math.floor(sortedArr.length / 2);
+    let right = left + 1;
+    let avg = (sortedArr[left] + sortedArr[right]) / 2;
+    return avg;
+  }
+}
+
+function _calculateMode(arr) {
+  let counts = frequencyCounter(arr);
+  let max = -Infinity;
+  let maxCount = -Infinity;
+
+  for (let n in counts) {
+    if (counts[n] > maxCount) {
+      max = n;
+      maxCount = counts[n];
+    }
+  }
+  return max;
+}
+
+function _frequencyCounter(arr) {
+  let count = {};
+
+  for (let num of arr) {
+    count[num] = (count[num] || 0) + 1;
+  }
+  return count;
+}
 
 app.use(function(req, res, next) {
   const notFoundError = new ExpressError("Not Found", 404);
